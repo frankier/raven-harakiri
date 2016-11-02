67430102b20922f10269225b16b8fd2d128318a4
Sean Davis <dive@endersgame.net>
2016-11-02 13:37:06 -0500

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class PyTest(setuptools.command.test.test):
 
 setup(
     name='raven-harakiri',
-    version='0.1.5',
+    version='0.1.6',
     py_modules=['raven_harakiri'],
     url='http://github.com/LexMachinaInc/raven-harakiri',
     license='MIT',
